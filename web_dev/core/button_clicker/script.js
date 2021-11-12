3212524c86720ffa1fcb3e03a314720674232a33
D5HARK <70493672+D5HARK@users.noreply.github.com>
2021-11-12 14:16:24 -0800

    completed!

---
+++ web_dev/core/button_clicker/script.js
@@ -0,0 +1,9 @@
+function login_logout(element) {
+    if (element.innerText = "Login"){
+        element.innerText = "Logout";
+    } 
+}
+
+function remove_definition(element){
+    element.remove();
+}
