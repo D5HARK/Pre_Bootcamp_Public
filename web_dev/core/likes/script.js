b471bedf5a05b500659e925837b271b8523e700c
D5HARK <70493672+D5HARK@users.noreply.github.com>
2021-11-12 18:36:24 -0800

    is done

--- web_dev/core/likes/script.js
+++ web_dev/core/likes/script.js
@@ -1,6 +1,15 @@
-function increase_like(){
-   var initial_like_value = document.querySelector(".like_counter");
-   console.log(typeof test);
+function increase_like_one(){
+   var initial_like_value = document.querySelector("#like_counter_one");
+   var new_like_value = parseInt(initial_like_value.innerHTML) + 1;
+   initial_like_value.innerHTML = new_like_value;
+}
+function increase_like_two(){
+   var initial_like_value = document.querySelector("#like_counter_two");
+   var new_like_value = parseInt(initial_like_value.innerHTML) + 1;
+   initial_like_value.innerHTML = new_like_value;
+}
+function increase_like_three(){
+   var initial_like_value = document.querySelector("#like_counter_three");
    var new_like_value = parseInt(initial_like_value.innerHTML) + 1;
    initial_like_value.innerHTML = new_like_value;
 }
