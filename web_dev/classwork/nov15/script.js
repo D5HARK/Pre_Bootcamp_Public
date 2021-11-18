51e8be9ca05b8abb5a1df21ba76c9fcf0a911fb7
D5HARK <70493672+D5HARK@users.noreply.github.com>
2021-11-17 17:13:05 -0800

    big ol commit

---
+++ web_dev/classwork/nov15/script.js
@@ -0,0 +1,16 @@
+function quick_mafs(){
+    var a = 25;
+    a = a - 13;
+    console.log(a/2);  
+    a = "hello";
+    console.log(a + " hello");
+}
+
+// function array_practice(give_me_an_array){
+//     var sum = array_of_numbers[0];
+
+//     for(var i = 0; i<give_me_an_array.length)
+// }
+
+quick_mafs()
+
