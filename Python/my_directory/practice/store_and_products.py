45bbf44aac172df64d26715b391f044800ceaaad
D5HARK <70493672+D5HARK@users.noreply.github.com>
2021-12-08 23:36:54 -0800

    works as long as we don't randomly generate two of the same numbers, will address soon

--- Python/my_directory/practice/store_and_products.py
+++ Python/my_directory/practice/store_and_products.py
@@ -1,15 +1,20 @@
+import random
+
+
 class Store:
-    def __init__(self, name, product_list):
+    def __init__(self, name):
         self.name = name
-        self.product_list = product_list
         self.product_list = []
 
-    def add_product(self, new_product, price, category):
-        self.product_list.append(Product(new_product, price, category))
+    def add_product(self, product):
+        self.product_list.append(product)
 
-    def sell_product(self, index):
-        self.product_list[index].print_info()
-        self.product_list.remove(index)
+    def sell_product(self, upc):
+        for item in self.product_list:
+            if item.u_id == upc:
+                item.print_info()
+                print(item.name, "sold for", item.price)
+                del item
 
     def inflation(self, percentage):
         for item in self.product_list:
@@ -25,6 +30,7 @@ class Product:
         self.name = name
         self.price = price
         self.category = category
+        self.u_id = random.randint(1000, 9999)
 
     def update_price(self, percent_change, is_increased):
         if is_increased:
@@ -33,10 +39,21 @@ class Product:
             self.price -= (self.price * percent_change)
 
     def print_info(self):
-        print("Name: ", self.name, "Category: ", self.category, "Price: ", self.price, sep="\n")
+        print("Name: ", self.name, "\n", "Category: ", self.category, "\n", "Price: ", self.price, "\n", "UPC: ",
+              self.u_id, sep="")
+
 
 def main():
-    sale_store = Store("sale store", stock)
+    sale_store = Store("sale store")
+    carrots = Product("carrots", 4.00, "veggie")
+    cereal = Product("cereal", 5.00, "breakfast")
+    sale_store.add_product(carrots)
+    sale_store.add_product(cereal)
+    sale_store.inflation(.5)
+    sale_store.clearance(.1)
+    for item in sale_store.product_list:
+        item.print_info()
+    sale_store.sell_product(int(input("UPC: ")))
 
 
 if __name__ == "__main__":
