ce3874dea3dffbf663356623c1db6f7b102d54f2
D5HARK <70493672+D5HARK@users.noreply.github.com>
2022-01-01 13:00:51 -0800

    counter complete

--- Python/flask/fundamentals/counter/server.py
+++ Python/flask/fundamentals/counter/server.py
@@ -1,7 +1,31 @@
 from flask import Flask, render_template, request, redirect, session
+from flask.templating import render_template_string
 
 app = Flask(__name__)
+app.secret_key = "0Wy*p*0YrR{7N.2v#a0Aw$0qJ"
 
+
+
+@app.route("/")
+def home():
+    if session["session_number"] >= 0:
+        session["session_number"] += 1
+    else:
+        session["session_number"] = 0
+        
+    return render_template("index.html")
+
+@app.route("/count", methods = ["POST"])
+def count_session():
+    return redirect("/")
+
+
+@app.route("/destroy_session", methods = ["POST"])
+def reset_session():
+    session.clear()
+    session["session_number"] = -1
+    
+    return redirect('/')
 
 
 
