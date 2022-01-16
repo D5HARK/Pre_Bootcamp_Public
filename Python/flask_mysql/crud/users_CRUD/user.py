9a56e4f450a8242b2b1ad3f724a4be012cc7133c
D5HARK <70493672+D5HARK@users.noreply.github.com>
2022-01-16 11:40:44 -0800

    updated bool solved, silly typo

--- Python/flask_mysql/crud/users_CRUD/user.py
+++ Python/flask_mysql/crud/users_CRUD/user.py
@@ -1,5 +1,6 @@
 from mysqlconnection import connecttoMySQL
 
+
 class User:
     def __init__(self, data):
         self.id = data["id"]
@@ -7,11 +8,12 @@ class User:
         self.last_name = data["last_name"]
         self.created_at = data["created_at"]
         self.updated_at = data["updated_at"]
+
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
         results = connecttoMySQL('users_schema').query_db(query)
         users = []
         for user in results:
-            users.append( cls(user) )
+            users.append(cls(user))
         return users
